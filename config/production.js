80d9a11fd7cb4f256047bf8e44448072a53d8d37
Peter <diversemix@gmail.com>
2018-06-26 23:33:50 +0100

    wip: Add the files created so far

---
+++ config/production.js
@@ -0,0 +1,32 @@
+const { deferConfig } = require('config/defer')
+const winston = require('winston')
+
+winston.level = 'debug'
+
+module.exports = {
+  'pubsweet-server': {
+    baseUrl: deferConfig(
+      cfg => `http://localhost:${cfg['pubsweet-server'].port}`,
+    ),
+    secret: 'not very secret',
+    graphiql: true,
+    logger: winston,
+    db: {
+      host: 'postgres'
+    }
+  },
+
+  dbManager: {
+    username: 'admin',
+    password: 'password',
+    email: 'admin@example.com',
+    admin: true,
+  },
+  'auth-orcid': {
+    // get the oauth credentials from another developer or
+    // create new ones at https://orcid.org/content/register-client-application-sandbox
+    clientID: '',
+    clientSecret: '',
+    sandbox: true,
+  },
+}
